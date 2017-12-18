05993a27d24c49cd0c43973c4d34c9694051b921
Mitchell M <mitchellm1618@gmail.com>
2017-12-17 21:31:33 -0800

    completed commen tasks

--- mitchell-sunitha/public/scripts/article.js
+++ mitchell-sunitha/public/scripts/article.js
@@ -55,7 +55,7 @@ Article.truncateTable = callback => {
 
 Article.prototype.insertRecord = function(callback) {
   $.post('/articles', {author: this.author, authorUrl: this.authorUrl, body: this.body, category: this.category, publishedOn: this.publishedOn, title: this.title})
-  .then(data => {
+    .then(data => {
     console.log(data);
     if (callback) callback();
   })
